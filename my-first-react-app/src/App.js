31cc7a6eb720a03d76b308be8934caf99de24ed3
Rafay Memon <rafayrashid200611@gmail.com>
2023-08-25 21:02:39 +0500

    push

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="">
+        <h1 className='name'>
+          <b>Abdul Rafay</b>
+        </h1>
+      </header>
+
+      <section className="">
+        <p>
+          <b>Abc Building, Xyz Road, P.O Box 00, Karachi, Sindh Pakistan</b>
+        </p>
+        <p>
+          <b>Mobile:</b> +92-3333333333, +92-3333333333, +92-3333333333
+        </p>
+        <p>
+          <b>E-mail:</b> <a href='rafayrashid200611@gmail.com'> rafayrashid200611@gmail.com</a>
+        </p>
+      </section>
+
+      <section className="">
+        <h2>
+          <u>OBJECTIVES</u>
+        </h2>
+        <p>
+          Intend to build a career with a leading corporate of hi-tech environment with committed & dedicated people, through constant learning, which will help me to explore myself and realize my potential. Willing to work as a key player in a challenging & creative environment.
+        </p>
+      </section>
+
+      <section className="">
+        <h2>
+          <u>PERSONAL PROFILE</u>
+        </h2>
+        <p>
+          <ul>
+            <li>Ability to work in a team</li>
+            <li>Good Interpersonal Relationship and Excellent Communication Skills</li>
+            <li>Strong Organizational Skills</li>
+            <li>Resourceful, dynamic, and hardworking</li>
+          </ul>
+        </p>
+      </section>
+
+      
+
+      <section className="">
+        <h2>
+          <u>EDUCATIONAL PROFILE</u>
+        </h2>
+        <ul>
+          <li>Matriculation from Raza Foundation School</li>
+          <li>Microsoft Office course from Rose Tech</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default App;
